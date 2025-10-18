747f872a12c4430a6f51bcaef6ff359fe64a54d5
Giulio Cappelli <giulio.cappelli1e@gmail.com>
2025-10-18 15:53:57 +0200

    added and updated places, updated navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,123 +1,123 @@
 import {
-  Center,
-  Navbar,
-  Stack,
-  Tooltip,
-  UnstyledButton,
-  createStyles,
-  rem,
-  useMantineTheme,
+	Center,
+	Navbar,
+	Stack,
+	Tooltip,
+	UnstyledButton,
+	createStyles,
+	rem,
+	useMantineTheme,
 } from "@mantine/core";
 import {
-  IconBeer,
-  IconPlus,
-  IconShoppingCart,
-  IconToolsKitchen2,
+	IconGlassCocktail,
+	IconPlus,
+	IconShoppingCart,
+	IconToolsKitchen2,
 } from "@tabler/icons-react";
 
 const useStyles = createStyles((theme) => ({
-  link: {
-    width: rem(50),
-    height: rem(50),
-    borderRadius: theme.radius.md,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: theme.white,
-    opacity: 0.85,
+	link: {
+		width: rem(50),
+		height: rem(50),
+		borderRadius: theme.radius.md,
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		color: theme.white,
+		opacity: 0.85,
 
-    "&:hover": {
-      opacity: 1,
-      backgroundColor: theme.fn.lighten(
-        theme.fn.variant({ variant: "filled", color: theme.primaryColor })
-          .background!,
-        0.1
-      ),
-    },
-  },
+		"&:hover": {
+			opacity: 1,
+			backgroundColor: theme.fn.lighten(
+				theme.fn.variant({ variant: "filled", color: theme.primaryColor })
+					.background!,
+				0.1
+			),
+		},
+	},
 
-  active: {
-    opacity: 1,
-    "&, &:hover": {
-      backgroundColor: theme.fn.lighten(
-        theme.fn.variant({ variant: "filled", color: theme.primaryColor })
-          .background!,
-        0.15
-      ),
-    },
-  },
+	active: {
+		opacity: 1,
+		"&, &:hover": {
+			backgroundColor: theme.fn.lighten(
+				theme.fn.variant({ variant: "filled", color: theme.primaryColor })
+					.background!,
+				0.15
+			),
+		},
+	},
 }));
 
 interface NavbarLinkProps {
-  icon: React.FC<any>;
-  label: string;
-  active?: boolean;
-  onClick?(): void;
+	icon: React.FC<any>;
+	label: string;
+	active?: boolean;
+	onClick?(): void;
 }
 
 const NavbarLink = ({
-  icon: Icon,
-  label,
-  active,
-  onClick,
+	icon: Icon,
+	label,
+	active,
+	onClick,
 }: NavbarLinkProps) => {
-  const { classes, cx } = useStyles();
-  return (
-    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
-      <UnstyledButton
-        onClick={onClick}
-        className={cx(classes.link, { [classes.active]: active })}
-      >
-        <Icon size="1.2rem" stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  );
+	const { classes, cx } = useStyles();
+	return (
+		<Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+			<UnstyledButton
+				onClick={onClick}
+				className={cx(classes.link, { [classes.active]: active })}
+			>
+				<Icon size="1.2rem" stroke={1.5} />
+			</UnstyledButton>
+		</Tooltip>
+	);
 };
 
 const selections = [
-  { icon: IconToolsKitchen2, label: "Mangiare" },
-  { icon: IconBeer, label: "Bere" },
-  { icon: IconPlus, label: "Aggiungi Nuovo" },
+	{ icon: IconToolsKitchen2, label: "Mangiare" },
+	{ icon: IconGlassCocktail, label: "Aperitivo" },
+	{ icon: IconPlus, label: "Aggiungi Nuovo" },
 ];
 
 export const NavbarMinimalColored = (props: {
-  active: number;
-  setActive: any;
+	active: number;
+	setActive: any;
 }) => {
-  const { active, setActive } = props;
+	const { active, setActive } = props;
 
-  const links = selections.map((link, index) => (
-    <NavbarLink
-      {...link}
-      key={link.label}
-      active={index === active}
-      onClick={() => setActive(index)}
-    />
-  ));
+	const links = selections.map((link, index) => (
+		<NavbarLink
+			{...link}
+			key={link.label}
+			active={index === active}
+			onClick={() => setActive(index)}
+		/>
+	));
 
-  const theme = useMantineTheme();
+	const theme = useMantineTheme();
 
-  return (
-    <Navbar
-      height={"100vh"}
-      width={{ base: 80 }}
-      p="md"
-      sx={{
-        backgroundColor: theme.primaryColor,
-        padding: 0,
-        overflow: "hidden",
-      }}
-    >
-      <Center>
-        <IconShoppingCart size={32} stroke={2} color={theme.white} />
-      </Center>
-      <Navbar.Section grow mt={50}>
-        <Stack justify="center" spacing={4}>
-          {links}
-        </Stack>
-      </Navbar.Section>
-    </Navbar>
-  );
+	return (
+		<Navbar
+			height={"100vh"}
+			width={{ base: 80 }}
+			p="md"
+			sx={{
+				backgroundColor: theme.primaryColor,
+				padding: 0,
+				overflow: "hidden",
+			}}
+		>
+			<Center>
+				<IconShoppingCart size={32} stroke={2} color={theme.white} />
+			</Center>
+			<Navbar.Section grow mt={50}>
+				<Stack justify="center" spacing={4}>
+					{links}
+				</Stack>
+			</Navbar.Section>
+		</Navbar>
+	);
 };
 
 /*
